4bcd80d27f8bef7018788eac388d62fc0e7c4e74
FastStim <student2063@yandex.ru>
2018-11-22 10:22:42 +0300

    add more status information

--- convert.py
+++ convert.py
@@ -61,20 +61,28 @@ def save_json(coco_json, output_json, debug):
     with open(output_json, 'w') as outfile:
         json.dump(coco_json, outfile)
 
-    print('>>> \033[33m[OK]\033[0m save json file')
+    print(' \033[33m[OK]\033[0m save json file')
 
 
-def add_json_images(input_json, debug):
+def add_json_images(input_json, all_count, debug):
     i = 0
     j = 0
+    count = 0
+    pos = 0
     images = []
     annotations = []
 
     for js in input_json:
+        pos += 1
         if not js['outputs']:
+            print('\033[31m [%04d/%04d]\033[0m %s \033[31mwrong\033[0m' % (pos, all_count, js['path'].rsplit('\\', 1)[1]))
             continue
 
+        print('\033[34m [%04d/%04d]\033[0m %s \033[34mcorrect\033[0m' % (pos, all_count, js['path'].rsplit('\\', 1)[1]))
+
+        count += 1
         i += 1
+
         # Filename may be need change to after
         image = {
             'id': i,
@@ -118,10 +126,12 @@ def add_json_images(input_json, debug):
             }
             annotations.append(annotation)
             if debug:
-                print('annotation: %s' % annotation)
+                print(' annotation: %s' % annotation)
 
         if debug:
-            print('image: %s' % image)
+            print(' image: %s' % image)
+
+    print('\033[33m count correct images:\033[0m %s' % count)
 
     return images, annotations
 
@@ -132,7 +142,7 @@ def create_json(debug):
     data = {}
     info = {
         'year': datetime.datetime.now().year,
-        'version': '0.01',
+        'version': '0.02',
         'description': 'Small smoke dataset',
         'contributor': 'FastStim',
         'url': 'faststim.com',
@@ -168,7 +178,7 @@ def create_json(debug):
     data['categories'] = cat
 
     if debug:
-        print('>>> data: %s' % data)
+        print(' data: %s' % data)
 
     return data
 
@@ -180,30 +190,34 @@ def parse_json(files, debug):
             data.append(json.load(f))
 
     if debug:
-        print('>>> data: %s' % data)
-        print('\033[35m>>> end function: parse_json\033[0m')
+        print(' data: %s' % data)
+        print('\033[35m end function: parse_json\033[0m')
 
     return data
 
 
 def main(input_dir, output, debug):
-    print('\033[1m\033[35m>>> start program\033[0m')
+    print('\033[1m\033[35m start program\033[0m')
     if debug:
-        print('>>> input_dir = "%s"' % input_dir)
+        print(' input_dir = "%s"' % input_dir)
 
     coco_json = create_json(debug)
 
     # Required to add a check that would take only json
     json_files = [input_dir + '/' + f for f in listdir(input_dir) if isfile(join(input_dir, f))]
+
+    all_count = len(json_files)
+    print('\033[33m boxx all find files:\033[0m %s' % all_count)
+
     if debug:
-        print('>>> files %s' % json_files)
+        print(' files %s' % json_files)
 
     input_json = parse_json(json_files, debug)
-    coco_json['images'], coco_json['annotations'] = add_json_images(input_json, debug)
+    coco_json['images'], coco_json['annotations'] = add_json_images(input_json, all_count, debug)
 
     save_json(coco_json, output, debug)
 
-    print('\033[1m\033[35m>>> end program\033[0m')
+    print('\033[1m\033[35m end program\033[0m')
 
 
 if __name__ == "__main__":
